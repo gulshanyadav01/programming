cbb423109ec325bd09d9b70b7e5b180ba4ec6746
ashwani65 <ashwanisinghlpu66@gmail.com>
2020-01-17 12:11:15 +0530

    adding repo

---
+++ swap.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+	cout<<(a+b)<<endl;
+	return 0;
+}
+//just for demo purpose for basic understandin of gi commands
